d6ffef4d74638362514bc9d3fba3514e16347b63
NguyenThiUoc <109397876+Toeeeee@users.noreply.github.com>
2022-12-09 20:56:18 +0700

    Color List

---
+++ 3. Color List/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colorlist;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    public static void main(String[] args) {
+        ListColor list  = new ListColor("Color Picker Using List") ;
+    }
+}
